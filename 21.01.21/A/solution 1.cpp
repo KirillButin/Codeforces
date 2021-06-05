efe402c882cf578482f710dafea279f3ab8883d3
Kirill Butin <kbxxi@yandex.ru>
2021-06-05 17:41:33 +0300

    add editorials

--- 21.01.21/A/solution 1.cpp
+++ 21.01.21/A/solution 1.cpp
@@ -1,26 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
 #include <algorithm>
-
-
+#include <vector>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <set>
+#include <map>
+#include <sstream>
+#include <cassert>
 using namespace std;
+#pragma comment(linker, "/STACK:20000000")
 
-int arr[100100];
+typedef vector<int> vi;
+#define sz(a) int((a).size()) 
+#define all(c) (c).begin(),(c).end() 
+
+
+long long t[100005];
+int mn[100005];
+int n;
+int res;
+int mx;
+// сумма, произведение, последнее число, сколько осталось поставить
+void go(long long s, long long p, int l, int n) {
+    if (n < 0) return;
+    if (n == 0) {
+        if (s == p) {
+            res++;
+            mx = max(mx, l);
+        }
+        return;
+    }
+
+    for (int i = l; i < 550; i++) {
+        if (p * i - s - i <= n)
+            go(s + i, p * i, i, n - 1);
+        else break;
+    }
+}
+
 
 int main() {
-	int res = 0;
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	sort(arr,arr+n);
-	for (int i = 0; i < n; i++){		
-		if (arr[i] > res + 1){
-			cout << res + 1;
-			return 0;
-		}
-		res += val;
-	}
-	cout << res + 1;
 
-	return 0;
+
+    cin >> n;
+
+    res = 0;
+    go(0, 1, 1, n);
+    cout << res << endl;
 }
